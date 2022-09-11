b25ae02c5f38ea7ea4c649ce753f0d5b55cb2c6b
Evgeniy Bakaev <evgeniy.bakaev@compvisionsys.com>
2022-09-11 17:41:59 +0300

    add get_graph_info & build_two_cycles_graph funcs

---
+++ project/graphLib.py
@@ -0,0 +1,20 @@
+import string
+import cfpq_data
+import networkx
+
+
+def get_graph_info(graph_name: string):
+    graph_path = cfpq_data.download(graph_name)
+    graph = cfpq_data.graph_from_csv(graph_path)
+    nodes_num = graph.number_of_nodes()
+    edges_num = graph.number_of_edges()
+    labels = []
+    for edge in graph.edges(data=True):
+        if edge[2]["label"] not in labels:
+            labels.append(edge[2]["label"])
+    return nodes_num, edges_num, labels
+
+
+def build_two_cycles_graph(n, m, labels, path):
+    graph = cfpq_data.labeled_two_cycles_graph(n, m, labels=labels)
+    networkx.drawing.nx_pydot.write_dot(graph, path)
---
+++ tests/task1/test_graphLib.py
@@ -0,0 +1,38 @@
+import filecmp
+import project.graphLib
+
+
+def test_get_graph_info():
+    nodes, edges, labels = project.graphLib.get_graph_info("generations")
+
+    # from README of generations.tar.gz
+    assert nodes == 129
+    assert edges == 273
+    assert labels == [
+        "rest",
+        "first",
+        "onProperty",
+        "type",
+        "hasValue",
+        "someValuesFrom",
+        "equivalentClass",
+        "intersectionOf",
+        "inverseOf",
+        "range",
+        "hasSibling",
+        "sameAs",
+        "hasParent",
+        "hasSex",
+        "hasChild",
+        "versionInfo",
+        "oneOf",
+    ]
+
+
+def test_build_two_cycles_graph():
+    project.graphLib.build_two_cycles_graph(
+        2, 3, ["a", "b"], "./tests/task1/builded_graph.dot"
+    )
+    assert filecmp.cmp(
+        "./tests/task1/builded_graph.dot", "./tests/task1/expected_graph.dot"
+    )
