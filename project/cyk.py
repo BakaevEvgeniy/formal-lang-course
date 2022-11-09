a43464df5ffc0eb10a51e5241d8fbf91c67b8461
Evgeniy Bakaev <evgeniy.bakaev@compvisionsys.com>
2022-11-09 21:54:59 +0300

    apply pre-cummit

--- project/cyk.py
+++ project/cyk.py
@@ -1,27 +1,28 @@
 from pyformlang.cfg import CFG
 
+
 def CYK(cfg: CFG, word: str):
     n = len(word)
     if n == 0:
         return cfg.generate_epsilon()
-    
+
     cnf = cfg.to_normal_form()
-    
+
     term_productions = [p for p in cnf.productions if len(p.body) == 1]
     var_productions = [p for p in cnf.productions if len(p.body) == 2]
-            
+
     d = []
-    
+
     for i in range(n):
         d.append([])
         for j in range(n):
             d[i].append(set())
-            
+
     for i in range(n):
         for prod in term_productions:
             if word[i] == prod.body[0].value:
                 d[i][i].update(prod.head.value)
-    
+
     for step in range(1, n):
         for i in range(n - step):
             j = i + step
@@ -34,5 +35,3 @@ def CYK(cfg: CFG, word: str):
                         d[i][j].update(prod.head.value)
 
     return cnf.start_symbol.value in d[0][n - 1]
-                        
-    
--- tests/task8/test_cyk.py
+++ tests/task8/test_cyk.py
@@ -1,8 +1,10 @@
 from project.cyk import CYK
 from pyformlang.cfg import CFG
 
+
 def test_syk():
-    cfg = CFG.from_text("""
+    cfg = CFG.from_text(
+        """
                         S -> B B
                         S -> C D
                         S -> epsilon
@@ -10,14 +12,15 @@ def test_syk():
                         B -> C D
                         C -> (
                         D -> B E
-                        D -> )    
+                        D -> )
                         E -> )
-                        """)
-    
+                        """
+    )
+
     accepted = ["()", "()()", "((()()))", "()()()()()(())"]
     rejected = ["((", "(()", "(()()()))", ")()()()()(())"]
     for word in accepted:
         assert CYK(cfg, word) == True
-        
+
     for word in rejected:
         assert CYK(cfg, word) == False
