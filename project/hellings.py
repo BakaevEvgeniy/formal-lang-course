f546c77e75ca0cc083dbb4dde36b25ab3b275ed9
Evgeniy Bakaev <evgeniy.bakaev@compvisionsys.com>
2022-11-23 22:04:12 +0300

    task10

--- project/hellings.py
+++ project/hellings.py
@@ -1,38 +1,11 @@
 from typing import Tuple
-import cfpq_data
 from networkx import MultiDiGraph
-from pyformlang.cfg import CFG, Variable
+from pyformlang.cfg import CFG
 
-from project.cfgLib import cfg_to_weak_normal_form, read_cfg_from_file
-
-
-def cfpq_with_helling(
-    graph: MultiDiGraph,
-    cfg: CFG,
-    start_nodes: set[int] = None,
-    final_nodes: set[int] = None,
-    start_var: Variable = Variable("S"),
-) -> set[Tuple[int, int]]:
-    if not start_nodes:
-        start_nodes = graph.nodes
-    if not final_nodes:
-        final_nodes = graph.nodes
-
-    result = set()
-    for u, var, v in hellings(cfg, graph):
-        if var == start_var and u in start_nodes and v in final_nodes:
-            result.add((u, v))
-    return result
+from project.cfgLib import cfg_to_weak_normal_form
 
 
-def hellings(cfg, graph) -> set[Tuple[int, str, int]]:
-    if isinstance(cfg, str):
-        cfg = read_cfg_from_file(cfg)
-
-    if isinstance(graph, str):
-        graph_path = cfpq_data.download(graph)
-        graph = cfpq_data.graph_from_csv(graph_path)
-
+def hellings(cfg: CFG, graph: MultiDiGraph) -> set[Tuple[int, str, int]]:
     nodes_num = graph.number_of_nodes()
     if nodes_num == 0:
         return set()
